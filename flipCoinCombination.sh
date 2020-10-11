d533258278bb79f9f66f15af5bbe1388e50f9675
hritu567 <hritumog96@gmail.com>
2020-10-11 21:31:45 +0530

    Added : singlet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,25 @@
-#!/bin/bash -x
-ran=$((RANDOM%2))
-if [ $ran -eq 1 ]
-  then 
- echo "Flipped coin is head"
-   else
- echo "Flipped coin is tail"
-fi
+#!/bin/bash
+read -p "enter the number of times coin should be tossed " num
+Head=0
+Tail=0
+for ((i=0; i<$num; i++))
+do
+	ran=$((RANDOM%2))
+	if [ $ran -eq 1 ]
+	then
+		 ((Head++))
+	else
+		 ((Tail++))
+	fi
+done
+
+declare -A Coin
+Coin[((Head))]=$Head
+Coin[((Tail))]=$Tail
+echo ${Coin[@]}
+
+tothead=$(($Head*100))
+headper=$(($tothead/$num))
+tottail=$(($Tail*100))
+tailper=$(($tottail/$num))
+echo "head percentage : $headper , tails percentage : $tailper "
